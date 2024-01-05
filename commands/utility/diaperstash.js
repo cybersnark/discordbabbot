6d13d1b8f02266aaed6c0149b06418c7f2d03b5e
tythefoxie <cybersnark1901@gmail.com>
2024-01-04 23:27:24 -0500

    Edited index.js to initialize the database upon first run Edited commands/utility/register.js to add a new user to the database Edited commands/utility/diaperstash.js to corrrectly pull the user's stash from the database Changed database layout to use less columns, will only use name, brand, and quantity Added error handling for add and remove commands in diaperstash.js Added code to verify user is registered before allowing them to use diaperstash.js Removed unused code from diaperstash.js

--- commands/utility/diaperstash.js
+++ commands/utility/diaperstash.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('database', 'user', 'password', {
@@ -10,84 +10,27 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	storage: 'babdb.sqlite',
 });
 
-const diapStash = sequelize.define('diaperstash', {
+const userdb = sequelize.define('user', {
 	name: {
 		type: Sequelize.STRING,
 		unique: true,
 	},
-	// ABUniverse Brands
-	space: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	littlePawz: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	simple: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	sdk: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	preSchool: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	cushies: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	peekABU: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	// Tykables Brands
-	overnights: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	cammies: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	galactic: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	littleBuilders: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	unicorn: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	littleRawrs: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	trestElites: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
+});
 
+const diapStash = sequelize.define('diaperstash', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	brand: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	quantity: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
 });
 
 function valueToName(brand, quantity) {
@@ -236,7 +179,10 @@ module.exports = {
 				.addIntegerOption(option =>
 					option
 						.setName('quantity')
-						.setDescription('The number of diapers to remove.'),
+						.setDescription('The number of diapers to remove.')
+						.setRequired(true)
+						.setMinValue(1)
+						.setMaxValue(180),
 				))
 		.addSubcommand(subcommand =>
 			subcommand
@@ -245,53 +191,69 @@ module.exports = {
 		),
 	async execute(interaction) {
 		if (interaction.options.getSubcommand() === 'add') {
-			const brand = interaction.options.getString('brand');
-			const quantity = interaction.options.getInteger('quantity');
-			const user = await diapStash.findOne({ where: { name: interaction.user.username } });
-			if (!user) {
+			const choiceBrand = interaction.options.getString('brand');
+			const choiceQuantity = interaction.options.getInteger('quantity');
+			const registeredUser = await userdb.findOne({ where: { name: interaction.user.username } });
+			const user = await diapStash.findOne({ where: { name: interaction.user.username, brand: choiceBrand } });
+			if (!registeredUser) {
 				await interaction.reply({ content: 'Oops! Looks like you\'re not registered, kiddo! Use /register to get started!', ephemeral: true });
 			}
-			else if (quantity > 180) {
+			else if (choiceQuantity > 180) {
 				return interaction.reply({ content: 'You can\'t add more than 180 diapers at a time.', ephemeral: true });
 			}
-			else if (quantity < 1) {
+			else if (choiceQuantity < 1) {
 				return interaction.reply({ content: 'You can\'t add less than 1 diaper at a time.', ephemeral: true });
 			}
-			await diapStash.increment({ [brand]: quantity }, { where: { name: interaction.user.username } });
+			if (!user || !user.brand) {
+				await diapStash.create({
+					name: interaction.user.username,
+					brand: choiceBrand,
+					quantity: choiceQuantity,
+				});
+				await diapStash.sync();
+				return interaction.reply({ content: `You have added ${choiceQuantity} ${valueToName(choiceBrand, choiceQuantity)} to your diaper stash.`, ephemeral: true });
+			}
+			else if (user.quantity >= 0) {
+				await diapStash.increment({ 'quantity': choiceQuantity }, { where: { name: interaction.user.username, brand: choiceBrand } });
+			}
+
 			await diapStash.sync();
 			// TODO: Create a function to translate the value of the user's choice into a more user friendly string for the reply.
-			await interaction.reply({ content: `You have added ${quantity} ${valueToName(brand, quantity)} to your diaper stash.`, ephemeral: true });
+			await interaction.reply({ content: `You have added ${choiceQuantity} ${valueToName(choiceBrand, choiceQuantity)} to your diaper stash.`, ephemeral: true });
 		}
 		if (interaction.options.getSubcommand() === 'remove') {
-			const brand = interaction.options.getString('brand');
-			const brandName = interaction.options.getValues('brand');
-			const quantity = interaction.options.getInteger('quantity');
-			const user = await diapStash.findOne({ where: { name: interaction.user.username } });
-			if (!user) {
+			const choiceBrand = interaction.options.getString('brand');
+			const choiceQuantity = interaction.options.getInteger('quantity');
+			const registeredUser = await userdb.findOne({ where: { name: interaction.user.username } });
+			const user = await diapStash.findOne({ where: { name: interaction.user.username, brand: choiceBrand } });
+			if (!registeredUser) {
 				await interaction.reply({ content: 'Oops! Looks like you\'re not registered, kiddo! Use /register to get started!', ephemeral: true });
 			}
-			else if (quantity > 180) {
-				return interaction.reply({ content: 'You can\'t remove more than 180 diapers at a time.', ephemeral: true });
+			if (!user || !user.brand) {
+				return interaction.reply({ content: 'You don\'t have any of that diaper in your stash.', ephemeral: true });
 			}
-			else if (quantity < 1) {
+			else if (choiceQuantity < 1) {
 				return interaction.reply({ content: 'You can\'t remove less than 1 diaper at a time.', ephemeral: true });
 			}
-			await diapStash.decrement({ [brand]: quantity }, { where: { name: interaction.user.username } });
+			else if (choiceQuantity > user.quantity) {
+				return interaction.reply({ content: 'You can\'t remove more diapers than you have.', ephemeral: true });
+			}
+
+			await diapStash.decrement({ 'quantity': choiceQuantity }, { where: { name: interaction.user.username, brand: choiceBrand } });
 			await diapStash.sync();
-			await interaction.reply({ content: `You have removed ${quantity} ${brandName} diapers from your stash.`, ephemeral: true });
+			await interaction.reply({ content: `You have removed  ${choiceQuantity} ${valueToName(choiceBrand, choiceQuantity)} from your stash.`, ephemeral: true });
 		}
 		if (interaction.options.getSubcommand() === 'list') {
 			const currentStash = await diapStash.findAll({
+				attributes: ['name', 'brand', 'quantity'],
 				where: {
-					[Sequelize.and]: {
-						name: interaction.user.username,
-						[Sequelize.col()]: { [Sequelize.ne] : 0 },
+					name: interaction.user.username,
+					quantity: {
+						[Sequelize.Op.gt]: 0,
 					},
 				},
-			},
-			);
-			console.log(currentStash);
-			const stashString = currentStash.map(t => `${t.name} has ${t.space} Space, ${t.littlePawz} Little Pawz, ${t.simple} Simple, ${t.sdk} Super Dry Kids, ${t.preSchool} PreSchool, ${t.cushies} Cushies, ${t.peekABU} PeekABU, ${t.overnights} Overnights, ${t.cammies} Cammies, ${t.galactic} Galactic, ${t.littleBuilders} Little Builders, ${t.unicorn} Unicorn, ${t.littleRawrs} Little Rawrs, and ${t.trestElites} Trest Elites.`).join('\n');
+			});
+			const stashString = currentStash.map(t => `${t.quantity} ${valueToName(t.brand, t.quantity)}`).join('\n') || 'No diapers in your stash.';
 			await interaction.reply({ content: `${stashString}`, ephemeral: true });
 		}
 	},
--- commands/utility/register.js
+++ commands/utility/register.js
@@ -16,85 +16,6 @@ const userdb = sequelize.define('user', {
 		unique: true,
 	},
 });
-const diapStash = sequelize.define('diaperstash', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	// ABUniverse Brands
-	space: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	littlePawz: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	simple: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	sdk: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	preSchool: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	cushies: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	peekABU: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	// Tykables Brands
-	overnights: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	cammies: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	galactic: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	littleBuilders: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	unicorn: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	littleRawrs: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-	trestElites: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 0,
-	},
-
-});
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -111,9 +32,6 @@ module.exports = {
 			await interaction.reply({ content: 'Welcome to the Nursery, kiddo! One moment while I get your name written down...', ephemeral: true });
 			await wait(3_000);
 			await userdb.create({
-				name: interaction.user.username,
-			});
-			await diapStash.create({
 				name: interaction.user.username,
 			});
 			await interaction.followUp({ content: 'All done! You\'re now registered!', ephemeral: true });
